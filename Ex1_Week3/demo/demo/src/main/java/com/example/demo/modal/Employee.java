e2ebf15e63df1c2624910a9df0c39d5bbbb43831
Hoang Bao Long <LeoLincoln9804@gmail.com>
2025-02-18 18:28:24 +0700

    Add files via upload

---
+++ Ex1_Week3/demo/demo/src/main/java/com/example/demo/modal/Employee.java
@@ -0,0 +1,39 @@
+package com.example.demo.modal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String city;
+
+    public Employee() {}
+
+    public Employee(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
